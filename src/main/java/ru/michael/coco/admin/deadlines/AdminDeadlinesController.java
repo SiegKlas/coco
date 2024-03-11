5ebd5e602406c1b2a09e528cb8ff29fa11f87c58
michael <m.plugin@g.nsu.ru>
2024-03-12 02:56:04 +0700

    deadline-pass wip 2

--- src/main/java/ru/michael/coco/admin/deadlines/AdminDeadlinesController.java
+++ src/main/java/ru/michael/coco/admin/deadlines/AdminDeadlinesController.java
@@ -3,65 +3,58 @@ package ru.michael.coco.admin.deadlines;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import ru.michael.coco.level_description.LevelDescription;
+import ru.michael.coco.level_description.LevelDescriptionDTO;
+import ru.michael.coco.level_description.LevelDescriptionMapper;
 import ru.michael.coco.level_description.LevelDescriptionService;
 import ru.michael.coco.topic_description.TopicDescription;
+import ru.michael.coco.topic_description.TopicDescriptionDTO;
+import ru.michael.coco.topic_description.TopicDescriptionMapper;
 import ru.michael.coco.topic_description.TopicDescriptionService;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin/deadlines")
 public class AdminDeadlinesController {
     private final TopicDescriptionService topicDescriptionService;
     private final LevelDescriptionService levelDescriptionService;
+    private final TopicDescriptionMapper topicDescriptionMapper;
+    private final LevelDescriptionMapper levelDescriptionMapper;
 
     @Autowired
-    public AdminDeadlinesController(TopicDescriptionService topicDescriptionService, LevelDescriptionService levelDescriptionService) {
+    public AdminDeadlinesController(TopicDescriptionService topicDescriptionService, LevelDescriptionService levelDescriptionService, TopicDescriptionMapper topicDescriptionMapper, LevelDescriptionMapper levelDescriptionMapper) {
         this.topicDescriptionService = topicDescriptionService;
         this.levelDescriptionService = levelDescriptionService;
+        this.topicDescriptionMapper = topicDescriptionMapper;
+        this.levelDescriptionMapper = levelDescriptionMapper;
     }
 
-    @GetMapping("/topic/{topicNumber}")
-    public String showTopicDeadlineForm(@PathVariable Integer topicNumber, Model model) {
-        TopicDescription topicDescription = topicDescriptionService.findTopicDescriptionByNumber(topicNumber).orElseThrow();
-        model.addAttribute("topic", topicDescription);
-        return "topic_deadline_form";
+    @GetMapping
+    public String deadlines() {
+        return "deadlines";
     }
 
-    @PostMapping("/topic/{topicNumber}")
-    public String updateTopicDeadline(@PathVariable Integer topicNumber, @RequestParam("deadline") String deadline, Model model) {
-        TopicDescription topicDescription = topicDescriptionService.findTopicDescriptionByNumber(topicNumber).orElseThrow();
-        topicDescription.setDeadLine(parseDate(deadline));
-        topicDescriptionService.save(topicDescription);
-        return "redirect:/";
+    @GetMapping("/global")
+    public String globalDeadlines(Model model) {
+        List<TopicDescription> topicDescriptionList = topicDescriptionService.findAllSorted();
+        List<TopicDescriptionDTO> topicDescriptionDTOS = topicDescriptionList.stream().map(topicDescriptionMapper::toDTO).toList();
+        model.addAttribute("topics", topicDescriptionDTOS);
+        return "deadlines";
     }
 
-    @GetMapping("/pass/level/{levelNumber}/topic/{topicNumber}")
-    public String showLevelPassForm(@PathVariable Integer levelNumber, @PathVariable Integer topicNumber, Model model) {
-        TopicDescription topicDescription = topicDescriptionService.findTopicDescriptionByNumber(topicNumber).orElseThrow();
-        LevelDescription levelDescription = topicDescription.getLevelDescriptions().stream()
-                .filter(level -> level.getNumber().equals(levelNumber))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("Level not found"));
-        model.addAttribute("topic", topicDescription);
-        model.addAttribute("level", levelDescription);
-        return "level_pass_form";
-    }
-
-    @PostMapping("/pass/level/{levelNumber}/topic/{topicNumber}")
-    public String updateLevelPass(@PathVariable Integer levelNumber, @PathVariable Integer topicNumber, @RequestParam("pass") Integer pass, Model model) {
-        TopicDescription topicDescription = topicDescriptionService.findTopicDescriptionByNumber(topicNumber).orElseThrow();
-        LevelDescription levelDescription = topicDescription.getLevelDescriptions().stream()
-                .filter(level -> level.getNumber().equals(levelNumber))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("Level not found"));
-        levelDescription.setPass(pass);
-        levelDescriptionService.save(levelDescription);
-        return "redirect:/";
+    @GetMapping("/levels")
+    @ResponseBody
+    public List<LevelDescriptionDTO> getLevelsForTopic(@RequestParam("topicNumber") Integer topicNumber) {
+        List<LevelDescription> levels = levelDescriptionService.findAllByTopicDescriptionNumberSorted(topicNumber);
+        return levels.stream().map(levelDescriptionMapper::toDTO).toList();
     }
 
     private Date parseDate(String date) {
