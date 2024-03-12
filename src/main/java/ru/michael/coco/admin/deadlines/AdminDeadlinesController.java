7cf6b544071154c6d51296acb1ed7e541369afcb
michael <m.plugin@g.nsu.ru>
2024-03-12 07:01:23 +0700

    deadline-pass global stable

--- src/main/java/ru/michael/coco/admin/deadlines/AdminDeadlinesController.java
+++ src/main/java/ru/michael/coco/admin/deadlines/AdminDeadlinesController.java
@@ -5,10 +5,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ru.michael.coco.level.Level;
+import ru.michael.coco.level.LevelService;
 import ru.michael.coco.level_description.LevelDescription;
 import ru.michael.coco.level_description.LevelDescriptionDTO;
 import ru.michael.coco.level_description.LevelDescriptionMapper;
 import ru.michael.coco.level_description.LevelDescriptionService;
+import ru.michael.coco.topic.Topic;
+import ru.michael.coco.topic.TopicService;
 import ru.michael.coco.topic_description.TopicDescription;
 import ru.michael.coco.topic_description.TopicDescriptionDTO;
 import ru.michael.coco.topic_description.TopicDescriptionMapper;
@@ -28,15 +32,19 @@ public class AdminDeadlinesController {
     private final UserService userService;
     private final TopicDescriptionService topicDescriptionService;
     private final LevelDescriptionService levelDescriptionService;
+    private final TopicService topicService;
+    private final LevelService levelService;
     private final UserMapper userMapper;
     private final TopicDescriptionMapper topicDescriptionMapper;
     private final LevelDescriptionMapper levelDescriptionMapper;
 
     @Autowired
-    public AdminDeadlinesController(UserService userService, TopicDescriptionService topicDescriptionService, LevelDescriptionService levelDescriptionService, UserMapper userMapper, TopicDescriptionMapper topicDescriptionMapper, LevelDescriptionMapper levelDescriptionMapper) {
+    public AdminDeadlinesController(UserService userService, TopicDescriptionService topicDescriptionService, LevelDescriptionService levelDescriptionService, TopicService topicService, LevelService levelService, UserMapper userMapper, TopicDescriptionMapper topicDescriptionMapper, LevelDescriptionMapper levelDescriptionMapper) {
         this.userService = userService;
         this.topicDescriptionService = topicDescriptionService;
         this.levelDescriptionService = levelDescriptionService;
+        this.topicService = topicService;
+        this.levelService = levelService;
         this.userMapper = userMapper;
         this.topicDescriptionMapper = topicDescriptionMapper;
         this.levelDescriptionMapper = levelDescriptionMapper;
@@ -74,11 +82,11 @@ public class AdminDeadlinesController {
     }
 
     @PostMapping("/global")
-    public ResponseEntity<String> processDeadlines(@RequestParam("topic") Integer topicNumber,
-                                                   @RequestParam("level") Integer levelNumber,
-                                                   @RequestParam("deadLine") Date deadLine,
-                                                   @RequestParam("passTopic") Integer passTopic,
-                                                   @RequestParam("passLevel") Integer passLevel) {
+    public ResponseEntity<String> processGlobalDeadlines(@RequestParam("topic") Integer topicNumber,
+                                                         @RequestParam("level") Integer levelNumber,
+                                                         @RequestParam("deadLine") Date deadLine,
+                                                         @RequestParam("passTopic") Integer passTopic,
+                                                         @RequestParam("passLevel") Integer passLevel) {
         try {
             LevelDescription levelDescription =
                     levelDescriptionService.findLevelDescriptionByTopicDescriptionNumberAndNumber(topicNumber, levelNumber).orElseThrow();
@@ -86,7 +94,32 @@ public class AdminDeadlinesController {
             levelDescription.getTopicDescription().setPass(passTopic);
             levelDescription.getTopicDescription().setDeadLine(deadLine);
             levelDescriptionService.save(levelDescription);
-            return ResponseEntity.ok("Data received successfully!");
+            return ResponseEntity.ok("Data received successfully");
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.badRequest().body("No such topic or level");
+        }
+    }
+
+    @PostMapping("/students")
+    public ResponseEntity<String> processStudentDeadlines(@RequestParam("topic") Integer topicNumber,
+                                                          @RequestParam("level") Integer levelNumber,
+                                                          @RequestParam("deadLine") Date deadLine,
+                                                          @RequestParam("passTopic") Integer passTopic,
+                                                          @RequestParam("passLevel") Integer passLevel,
+                                                          @RequestParam("user") String userName) {
+        try {
+            User user = userService.findByUsername(userName).orElseThrow();
+            LevelDescription levelDescription =
+                    levelDescriptionService.findLevelDescriptionByTopicDescriptionNumberAndNumber(topicNumber, levelNumber).orElseThrow();
+            TopicDescription topicDescription = levelDescription.getTopicDescription();
+            Level level = levelService.findLevelByUserAndLevelDescription(user, levelDescription).orElseThrow();
+            Topic topic = topicService.findTopicByUserAndTopicDescription(user, topicDescription).orElseThrow();
+            level.setPass(passLevel);
+            topic.setPass(passTopic);
+            topic.setDeadLine(deadLine);
+            levelService.save(level);
+            topicService.save(topic);
+            return ResponseEntity.ok("Data received successfully");
         } catch (NoSuchElementException e) {
             return ResponseEntity.badRequest().body("No such topic or level");
         }
