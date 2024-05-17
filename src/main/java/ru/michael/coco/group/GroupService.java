0663cef19bbd7dba680cd3fd8a7be0b667aa1bd3
michael <m.plugin@g.nsu.ru>
2024-05-18 06:37:41 +0700

    banks dirs

--- src/main/java/ru/michael/coco/group/GroupService.java
+++ src/main/java/ru/michael/coco/group/GroupService.java
@@ -1,18 +1,27 @@
 package ru.michael.coco.group;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class GroupService {
     private final GroupRepository groupRepository;
+    private final String groupsDir;
 
     @Autowired
-    public GroupService(GroupRepository groupRepository) {
+    public GroupService(GroupRepository groupRepository, @Value("${file.groups-dir}") String groupsDir) {
         this.groupRepository = groupRepository;
+        this.groupsDir = groupsDir;
     }
 
     public List<Group> findAllGroups() {
@@ -25,14 +34,41 @@ public class GroupService {
 
     public void saveGroup(Group group) {
         groupRepository.save(group);
+        createGroupDirectory(group.getId());
     }
 
     public void deleteGroupById(Long id) {
         groupRepository.deleteById(id);
+        deleteGroupDirectory(id);
     }
 
     public Optional<Group> findByName(String name) {
         return groupRepository.findByName(name);
+    }
+
+    private void createGroupDirectory(Long groupId) {
+        Path groupPath = Paths.get(groupsDir, String.valueOf(groupId));
+        try {
+            if (!Files.exists(groupPath)) {
+                Files.createDirectories(groupPath);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to create directory for group " + groupId, e);
+        }
+    }
+
+    private void deleteGroupDirectory(Long groupId) {
+        Path groupPath = Paths.get(groupsDir, String.valueOf(groupId));
+        try {
+            if (Files.exists(groupPath)) {
+                Files.walk(groupPath)
+                        .sorted(Comparator.reverseOrder())
+                        .map(Path::toFile)
+                        .forEach(File::delete);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to delete directory for group " + groupId, e);
+        }
     }
 }
 
